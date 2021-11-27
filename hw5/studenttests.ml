675c0779ed93989ac4ee8477168f54d116cf8c6e
ftchann <yannhuynh0@gmail.com>
2021-11-27 09:31:28 +0100

    added a few test

--- hw5/studenttests.ml
+++ hw5/studenttests.ml
@@ -6,6 +6,15 @@ open Gradedtests
 (* You should also add additional test cases here to help you   *)
 (* debug your program.                                          *)
 
+let tc_err_tests = [
+  "ourprograms/while_rettype.oat";
+  "ourprograms/struct_type.oat"
+]
+
+let tc_ok_tests = [
+  "ourprograms/if_rettype.oat"
+]
+
 let global_struct_test = [
   ("ourprograms/changed_order_global_struct.oat", "", "254");
   ("ourprograms/changed_order_normal_struct.oat", "", "8");
@@ -13,6 +22,8 @@ let global_struct_test = [
 ]
 
 let provided_tests : suite = [
+  Test("tc_ok_tests",typecheck_file_correct tc_ok_tests);
+  Test("tc_err_tests", typecheck_file_error tc_err_tests);
   Test ("Global struct Test", executed_oat_file global_struct_test)
   
 ] 
